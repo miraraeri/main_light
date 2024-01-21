efbf22651be35867b878a9218dae3b9fd290a3d6
miraraeri <155308644+miraraeri@users.noreply.github.com>
2024-01-21 19:46:19 +0300

    Test 2.2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title('Наш новый проект')
-button_pressed = st.button("Рассчитать возможность получения займа")
 flag = False
+button_pressed = st.button("Рассчитать возможность получения займа")
 if button_pressed:
   flag = not flag
 if flag:
