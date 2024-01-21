0a9a3e74c449ea8da5a4e528f500c860dbbc940f
miraraeri <155308644+miraraeri@users.noreply.github.com>
2024-01-21 19:41:28 +0300

    Test 1

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import streamlit as st
 st.title('Наш новый проект')
+button_pressed = st.button("Рассчитать возможность получения займа")
+
+if button_pressed:
+  st.text('Исход 1')
+else:
+  st.text('Исход 2')
