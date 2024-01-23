7d125ab27f4d2029cc18142029a5303ab813e1de
miraraeri <155308644+miraraeri@users.noreply.github.com>
2024-01-23 20:35:24 +0300

    Тест кнопки

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ flag = False
 button_pressed = st.button("Рассчитать возможность получения займа")
 if button_pressed:
   flag = not flag
-  if flag:
-    st.text('Исход 1сыршвсршыостошыв')
-  else:
-    st.text('Исход 2мавировыиарыимиаым')
+if flag:
+  st.text('Исход 1сыршвсршыостошыв')
+else:
+  st.text('Исход 2мавировыиарыимиаым')
