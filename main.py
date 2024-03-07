ec7feef755d1d1eb1618d0c90b5c442c88f74a8f
Dzamila10 <157304203+Dzamila10@users.noreply.github.com>
2024-03-07 21:30:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,17 +14,20 @@ text_input[2] = st.text_input("Введите ширина помещения", 
 text_input[3] = st.text_input(  "Введите расстояние между потенциальным светильником и поверхностью, на которую падает свет", 
   "Введите количество метров")
 text_input[4] = st.text_input("Введите показатель световой отдачи", "Введите количество люксов")
+text_input[5] = st.text_input("Введите световой поток", "в люменах")
+
 
 text_input[1] = text_input[1].replace(',', ".")
-text_input[2] = text_input[2].replace(',', ".")
+text_input[2] = text_input[2].replаce(',', ".")
 text_input[3] = text_input[3].replace(',', ".")
 text_input[4] = text_input[4].replace(',', ".")
+text_input[5] = text_input[5].replace(',', ".")
 
 # ключевые показатели для нас разрабов): S - площадь помещения (м^2), A - длина помещения (м)
 # B - ширина помещения (м), H - расстояние между потенциальным светильником и поверхностью,
 # на которую падает свет (м), F - показатель световой отдачи или тип и мощность лампы 
 #E = (η * F) / S # расчитывает освещенность
-#N=(E*S)/(U*Ф*Кз) # кол-во светильников
+#N= E * S *k/ f # кол-во светильников
 #i=S/(h - h1)*(a + b) #индекс
 button_pressed = st.button("Рассчитать освещенность и количество светильников")
  
@@ -34,7 +37,12 @@ if button_pressed:
             s =float(text_input[2]) * float(text_input[1])
             i = s / (float(text_input[3])) * (float(text_input[2]) + float(text_input[1]))
             e = (i * float(text_input[4])) / s
-            n = (e * s) / (1 * 345 * 1.1)
+            f = float(text_input[5])
+            n = 0
+            if s <= 100 and float(text_input[3]) <= 2.7:
+                n = (e * s * 2.5) / f
+            else:
+                n = (e * s * 3) / f
             st.text(f'Освещенность: {e}')
             st.text(f'Количество ламп: {n}')
         else:
